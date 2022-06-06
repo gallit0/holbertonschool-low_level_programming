d8d85fa61bb6c0e9a5820c70485f121817be6966
gallit0 <mateogesede@gmail.com>
2022-06-06 10:08:21 -0700

    1

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -3,7 +3,7 @@
  *main - program that prints the size of various types on the computer it is compiled and run on
  *Return: 0
  */
-int main (void)
+int main(void)
 {
 	printf("Size of a char: ", sizeof(char), "\n");
 	printf("Size of an int: ", sizeof(int), "\n");
