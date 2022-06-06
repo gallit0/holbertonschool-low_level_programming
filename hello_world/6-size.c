7e4808dbd69a9a2891fc806a24a40ba721cd125e
gallit0 <mateogesede@gmail.com>
2022-06-06 10:37:13 -0700

    1

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -17,9 +17,4 @@ int main(void)
 	printf(" byte(s)\n");
 	
 	return (0);
-	/*printf("Size of a char: %lu", sizeof(char), " byte(s)\n");
-		        printf("Size of an int: %lu", sizeof(int), " byte(s)\n");
-				        printf("Size of a long int: %lu", sizeof(long int), " byte(s)\n");
-						        printf("Size of a long long int: %lu", sizeof(long long int), " byte(s)\n");
-								        printf("Size of a float: %lu", sizeof(float), " byte(s)\n");*/
 }
