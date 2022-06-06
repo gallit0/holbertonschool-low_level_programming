b6dd7fac24324f27603d31da04f3de017498e5c2
gallit0 <mateogesede@gmail.com>
2022-06-06 09:13:45 -0700

    1

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /*
- *main function includes the following
+ * main function includes the following
  * uses put to put a string
- * returns 0
+ * Returns 0
  */
 int main(void)
 {
