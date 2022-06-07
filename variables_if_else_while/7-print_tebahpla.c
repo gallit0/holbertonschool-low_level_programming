92dc258a20a4e64df79dbeff48d53915d35e0856
gallit0 <mateogesede@gmail.com>
2022-06-07 14:32:43 -0700

    1

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - alphabet reversed
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 'z'; i => 'a'; i--)
+	{
+		putchar(i);
+	}
+	putchar(10);
+	return (0);
+}
