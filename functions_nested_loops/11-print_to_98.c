89354f6c55d690feed7ef36a83ef866ab7911309
gallit0 <mateogesede@gmail.com>
2022-06-09 10:51:32 -0700

    1

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -11,15 +11,21 @@ void print_to_98(int n)
 		printf("%d", n); 
 		if( n < 98)
 		{
+			printf("%d", n);
 			n++;
 			_putchar(',');
 			_putchar(' ');
 		}
 		else if (n > 98)
 		{
+			printf("%d", n);
 			n--;
 			_putchar(',');
 			_putchar(' ');
+		}
+		else
+		{
+			printf("%d\n", n);
 		}
 	} while(n != 98);
 }
