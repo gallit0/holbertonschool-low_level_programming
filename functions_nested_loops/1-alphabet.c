ea95bd22cfcf0d0f1f314de1f0bd8ba9c49eb5f1
gallit0 <mateogesede@gmail.com>
2022-06-08 17:42:33 -0700

    1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - function alphabet
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char i = 'a';
+	
+	for (; i <='z'; i++)
+	{
+		putchar(i);
+		}
+	return;
+}
+
+
