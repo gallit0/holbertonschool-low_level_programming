1f2343e174fbd06dea17c7beb5a72cdb04331ba8
gallit0 <mateogesede@gmail.com>
2022-06-09 09:17:12 -0700

    1

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,9 +1,31 @@
 #include "main.h"
-#include <stdio.h>
 
+/**
+ * jack_bauver - 24
+ * @i: hours
+ * @j: mins
+ * Return: 0
+ */
 
 void jack_bauer(void)
 {
 	int i, j;
-	for (i = 0; 
+	for (i = 0; i < 24; i++)
+	{
+		for (j = 0; j < 60; j++)
+		{
+			if (i < 10)
+			{
+				_putchar(0 + '0');
+			}
+			_putchar(i + '0');
+			_putchar(':');
+			if (j < 10)
+			{
+				_putchar(0 + '0');
+			}
+			putchar(j + '0');
+			putchar(10);
+		}
+	}
 }
