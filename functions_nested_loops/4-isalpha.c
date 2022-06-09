1662155f4bdd579d920285df99e045a8060394d0
gallit0 <mateogesede@gmail.com>
2022-06-09 06:36:13 -0700

    1

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks alphabetic characters
+ * @c - character
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'));
+		return (1);
+	else;
+		return (0);
+}
