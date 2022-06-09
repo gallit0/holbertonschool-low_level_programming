5bfa6df27b7d5e442e00e82c4287117fceee2068
gallit0 <mateogesede@gmail.com>
2022-06-09 06:51:45 -0700

    1

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if ( n == 0)
+	{
+		_ptuchar(0 + '0');
+		return (0);
+	}
+	else
+	{
+		_putchar('n');
+		return (-1);
+	}
+}
