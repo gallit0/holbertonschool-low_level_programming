3c4c173a6e250a021c711da00515f2f704cb059c
gallit0 <mateogesede@gmail.com>
2022-06-09 06:08:02 -0700

    1

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _islower - checks whether its lower or not
+ */
+
+int _islower(int c)
+{
+	if (isupper(c));
+		return (0);
+	else;
+		return (1);
+}
