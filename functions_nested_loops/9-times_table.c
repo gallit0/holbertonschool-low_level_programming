7415f22e5db87dc78b647a99736ec395414d3a45
gallit0 <mateogesede@gmail.com>
2022-06-09 09:56:13 -0700

    1

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "main"
+
+
+
+void times_table(void)
+{
+	int filas, columnas; num;
+	
+	for (filas = 0; filas < 10; filas++)
+	{
+		for (columnas = 0; columnas < 10; columnas++)
+		{
+			num = filas * columnas;
+			if ((num / 10) != 0)
+			{
+				_putchar((num / 10) + '0');
+			}
+			else{
+				_putchar(' ');
+			}
+			_putchar((num % 10) + '0');
+
+			if (columnas < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		putchar(10);
+	}
+}
