ff05f4dbc1049f58a739fba11ca097f71c19cdd5
gallit0 <mateogesede@gmail.com>
2022-06-11 12:09:59 -0700

    1

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - fizzbuzz
+ * @i - iterations
+ * Return: 0;
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 15 == 0)
+		{
+			printf("FizzFuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else{
+			printf(i);
+		}
+		if (i < 100){
+			printf(" ");
+		}
+	}
+	printf("\n");
+	return (0);
+}
