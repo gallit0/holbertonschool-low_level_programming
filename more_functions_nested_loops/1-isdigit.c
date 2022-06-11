aef587c1dc3c67e06f08eb66b45cefe6b7e03497
gallit0 <mateogesede@gmail.com>
2022-06-11 10:44:56 -0700

    1

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -3,12 +3,12 @@
  * _isdigit - checks for number
  * @c: number
  * Return: 0 or 1
- * */
+ */
 
 
 int _isdigit(int c)
 {
-	if (c >= 48 && c <= 58)
+	if (c >= '0' && c <= '9')
 	{
 		return (1);
 	}
