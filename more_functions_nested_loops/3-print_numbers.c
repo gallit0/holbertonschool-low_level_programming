56d6d0665fdb1e0c721229993056f78dab8847a2
gallit0 <mateogesede@gmail.com>
2022-06-11 11:00:03 -0700

    1

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
  * print_numbers - numbers from 0 to 9
- * @i: iterations
+ * @i - iterations
  */
 
 void print_numbers(void)
 {
 	int i;
+
 	for (i = 0; i < 10; i++)
 	{
 		_putchar(i + '0');
