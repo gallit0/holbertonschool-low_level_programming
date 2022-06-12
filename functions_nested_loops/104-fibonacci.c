0674e52a6efd1117ad929cd7c94048c183084917
gallit0 <mateogesede@gmail.com>
2022-06-12 13:12:29 -0700

    1

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -11,9 +11,9 @@
 
 int main(void)
 {
-	unsigned long int currentNumber = 1;
-	unsigned long int previousNumber = 0;
-	unsigned long int temp;
+	unsigned int currentNumber = 1;
+	unsigned int previousNumber = 0;
+	unsigned int temp;
 	int i;
 
 	for (i = 1; i <= 98; i++)
