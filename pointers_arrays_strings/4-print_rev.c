779d910cb2648bbc03fbdc8583254048df0d97ba
gallit0 <mateogesede@gmail.com>
2022-06-14 13:09:46 -0700

    1

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+
+void print_rev(char *s)
+{
+	int i;
+	int length = _strlenght(s);
+
+        for (i = lenght; *(str + i) != '\0'; i--)
+	{
+		
+	        _putchar(*(str + i));
+	}
+	_putchar('10');
+}
