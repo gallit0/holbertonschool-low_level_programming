cea6242392734d108ab53d47ac2120177413ebc7
gallit0 <mateogesede@gmail.com>
2022-06-15 11:52:33 -0700

    1

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -8,14 +8,14 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	char temp = dest;
+	char temp = *dest;
 
 	while (*src != 0)
 	{
-		*dest = src
+		*dest = *src
 		dest++;
 		src++;
 	}
 	*dest = 0
-	return (0);
+	return (temp);
 }
