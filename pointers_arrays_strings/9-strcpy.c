75534601288716e7d326f5710f2b7e71c189ff9d
gallit0 <mateogesede@gmail.com>
2022-06-15 11:51:10 -0700

    1

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcpy - copy string
+ * @dest: string
+ * @src: string
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	char temp = dest;
+
+	while (*src != 0)
+	{
+		*dest = src
+		dest++;
+		src++;
+	}
+	*dest = 0
+	return (0);
+}
