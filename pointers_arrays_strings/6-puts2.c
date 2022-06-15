9837db9b0a65fe97c4ee5539798b19121e59ca13
gallit0 <mateogesede@gmail.com>
2022-06-15 10:01:40 -0700

    1

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -9,12 +9,9 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; (str[i]) != 0; i++)
+	for (i = 0; (str[i]) != 0; i + 2)
 	{
-		if ((str[i]) % 2 == 0)
-		{
-			putchar((str[i]));
-		}
+		putchar((str[i]));
 	}
 	putchar(10);
 }
