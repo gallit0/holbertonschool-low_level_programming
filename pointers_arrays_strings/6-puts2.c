79fe51e3b04266f77cf4887a2b36f03a4e43ee75
gallit0 <mateogesede@gmail.com>
2022-06-15 09:57:29 -0700

    1

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - put even characters
+ * @str: string
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; *str[i] != 0; i++)
+	{
+		if (*str[i] % 2 == 0)
+		{
+			putchar(*str[i] + '0');
+		}
+	}
+	putchar(10);
+}
