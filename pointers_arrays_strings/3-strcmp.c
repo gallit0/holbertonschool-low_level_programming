395058d09d557bd5ef9330647b39a487a20f4187
gallit0 <mateogesede@gmail.com>
2022-06-16 13:58:31 -0700

    1

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -12,10 +12,23 @@ int _strcmp(char *s1, char *s2)
 	int i;
 	int counter = 0;
 	int temp;
+	int len1 = strlen(s1);
+	int len2 = strlen(s2);
 
 	for (i = 0; (s1[i] != 0) && (s2[i] != 0); i++)
 	{
-		temp = s1[i] - s2[i];
+		if (i > len1)
+		{
+			temp = -s2[i];
+		}
+		if else (i < len2)
+		{
+			temp = s1[i];
+		}
+		else
+		{
+			temp = s1[i] - s2[i];
+		}
 		counter += temp;
 	}
 	return (counter);
