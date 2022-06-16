f2d1dc37f0507639adcfff0a6e4f382cb9a77c47
gallit0 <mateogesede@gmail.com>
2022-06-16 09:30:19 -0700

    1

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -9,7 +9,7 @@ char *_strncat(char *dest, char *src, int n)
 	int i;
 	int j = 0;
 
-	for (i = destTotalChars; i < totalChars && i < n; i++)
+	for (i = destTotalChars; i < n; i++)
 	{
 		dest[i] = src[j];
 		j++;
