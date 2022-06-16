50fe77686b4d616a48f2a451077a79e1a5102766
gallit0 <mateogesede@gmail.com>
2022-06-16 13:53:36 -0700

    1

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,6 +1,10 @@
 #include "main.h"
 
-
+/**
+ * _strcmp - differenciate
+ * @s1: string
+ * @s2: string
+ * Return: difference
 
 int _strcmp(char *s1, char *s2)
 {
@@ -8,7 +12,7 @@ int _strcmp(char *s1, char *s2)
 	int counter = 0;
 	int temp;
 
-	for (i = 0; s1[i] || s2[i]; i++)
+	for (i = 0; (s1[i] != 0) || (s2[i] != 0); i++)
 	{
 		temp = s1[i] - s2[i];
 		counter += temp;
