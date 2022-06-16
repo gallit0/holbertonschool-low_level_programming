390744c59d51955937c03409a023bd22a5e302d1
gallit0 <mateogesede@gmail.com>
2022-06-16 14:46:09 -0700

    1

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -6,9 +6,13 @@ char *cap_string(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != 0; i++)
+	if (str[i] >= 'a' && str[i] <= 'z')
 	{
-		if (str[i - 1] == 'A')
+		str[i] = str[i] - 32;
+	}
+	for (i = 1; str[i] != 0; i++)
+	{
+		if (str[i - 1] == ' ')
 		{
 			str[i] = str[i] - 32;
 		}
