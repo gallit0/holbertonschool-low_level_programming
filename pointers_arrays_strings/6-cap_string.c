d7ed816a0a9e91de73eb712dfacdb5766cac125f
gallit0 <mateogesede@gmail.com>
2022-06-16 15:15:59 -0700

    1

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -19,7 +19,7 @@ char *cap_string(char *str)
 	{
 		prev = i - 1;
 		/*if (str[i] >= 'a' && str[i] <= 'z' && (str[prev] == '	' || str[prev] ==' ' || str[prev] =='\n' || str[prev] == ',' || str[prev] == ';' || str[prev] == '.' || str[prev] == '!' || str[prev] == '"' || str[prev] == '(' || str[prev] == ')' || str[prev] == '{' || str[prev] == '}'))*/
-		if (str[i] >= 'a' && str[i] <= 'z' && (str[prev] <= 47 || (str[prev] >= 58 && str[prev] <= 64) || str[prev] >= 124))
+		if (str[i] >= 'a' && str[i] <= 'z' && isalpha(str[i]))
 		{
 			str[i] = str[i] - 32;
 		}
