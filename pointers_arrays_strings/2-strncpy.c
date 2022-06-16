2427eac284e105d3348c83f08c40287c6f2059d5
gallit0 <mateogesede@gmail.com>
2022-06-16 12:15:48 -0700

    1

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -10,11 +10,15 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
+	size_t i;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n && src[i] != 0; i++)
 	{
-		dest[i] = src[i];
+		dest[i] = src [i];
 	}
-	return (src);	
+	while (i < n)
+	{
+		dest[i++] = 0;
+	}
+	return (dest);
 }
