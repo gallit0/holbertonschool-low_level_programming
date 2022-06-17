c93a3b72aa1a44eeae2f9f71ec178b6065a30bd8
gallit0 <mateogesede@gmail.com>
2022-06-17 09:30:42 -0700

    1

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -22,3 +22,4 @@ char *string_toupper(char *str);
 char *cap_string(char *);
 char *leet(char *str);
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
