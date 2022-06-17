27c72962117a11674d3db78b0a8a2096660dabfc
gallit0 <mateogesede@gmail.com>
2022-06-17 14:22:30 -0700

    1

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -12,18 +12,8 @@ char *_strpbrk(char *s, char *accept)
 	if ((*s == 0) || (*accept == 0))
 		return (0);
 	do {
-		if (_strchr(*s, accept))
-				return (s);
-	} while (*s++);
-	return (0);
-}
-char *_strchr(char *s, char c)
-{
-	do {
-		if (*s == c)
-		{
+		if (strchr(s, *accept))
 			return (s);
-		}
 	} while (*s++);
 	return (0);
 }
