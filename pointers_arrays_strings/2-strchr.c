dbecf9be1e12617ec0e9648df73ee007348fa10f
gallit0 <mateogesede@gmail.com>
2022-06-17 09:42:27 -0700

    1

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+
+
+char *_strchr(char *s, char c)
+{
+	char *s1 = s;
+	int i = 0;
+
+	while (s != c)
+	{
+		++s;
+		++i;
+	}
+	if (s == c)
+	{
+		return s1 + i;
+	}
+	else
+	{
+		return (0);
+	}
+}
