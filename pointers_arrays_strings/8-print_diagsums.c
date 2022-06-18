f58e7125c59ddb8daa1746410b620721a97c90e7
gallit0 <mateogesede@gmail.com>
2022-06-18 10:38:08 -0700

    1

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -8,12 +8,13 @@
 
 void print_diagsums(int *a, int size)
 {
-	int i, j, m, n;
+	int i, j;
 	int result0 = 0;
 	int result1 = 0;
 
 	for (i = 0, j = size; i < size; i++, j++)
 	{
-
+		result0 += a[i][i];
+		result1 += a[j][j];
 	}
 }
