65792a7fb0d8afbd1f94e26573de6ee0e2a88527
gallit0 <mateogesede@gmail.com>
2022-06-18 10:35:06 -0700

    1

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_diagsums - print sum of two diagonals
+ * @a: int
+ * @size: int
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i, j, m, n;
+	int result0 = 0;
+	int result1 = 0;
+
+	for (i = 0, j = size; i < sizei++, j++)
+	{
+
+	}
+}
