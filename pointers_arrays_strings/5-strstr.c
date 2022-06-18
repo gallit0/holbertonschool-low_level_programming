a8ffd47ca5ece86ff3c5457a4e6cdb9a055732df
gallit0 <mateogesede@gmail.com>
2022-06-18 10:11:37 -0700

    1

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,8 +1,21 @@
 #include "main.h"
 
-
+/**
+ * _strstr - find first occurrence
+ * @haystack: string
+ * @needle: string
+ * Return: string
+ */
 
 char *_strstr(char *haystack, char *needle)
 {
-	
+	int sizeOfNeedle = strlen(needle);
+
+	while (*haystack)
+	{
+		if (!memcmp(haystack, needle, sizeOfNeedle))
+			return (haystack);
+		haystack++;
+	}
+	return (0);
 }
