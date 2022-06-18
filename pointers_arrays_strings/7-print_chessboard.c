6ce2048069767380893342bbe5f43bff577ddb3f
gallit0 <mateogesede@gmail.com>
2022-06-18 10:20:18 -0700

    1

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_chessboard - print the chessboard
+ * @a: string
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, k;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			putchar(a[i][j];
+		}
+		putchar(10);
+	}
+}
