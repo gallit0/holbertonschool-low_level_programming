d0554e99869f6347d981d631f63d80b9d063286d
gallit0 <mateogesede@gmail.com>
2022-06-19 11:31:02 -0700

    1

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -18,9 +18,9 @@ void print_diagsums(int *a, int size)
 		for (; j < size; j++)
 		{
 			if (i == j)
-				result0 += *((int *)a + i * size + j);
-			if (j == (size - i))
-				result1 += *((int *)a + i * size + j);
+				result0 += (*((int *)a + i * size + j));
+			if (j == (size - 1 - i))
+				result1 += (*((int *)a + i * size + j));
 		}
 	}
 	printf("%d, %d\n", result0, result1);
