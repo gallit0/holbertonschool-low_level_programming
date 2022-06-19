058c0ada6ba2b263fa1098a8833b2a07857e7124
gallit0 <mateogesede@gmail.com>
2022-06-19 11:31:36 -0700

    1

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -10,8 +10,8 @@ void print_diagsums(int *a, int size)
 {
 	int i = 0;
 	int j = 0;
-	int result0 = 0;
-	int result1 = 0;
+	unsigned int result0 = 0;
+	unsigned int result1 = 0;
 
 	for (; i < size; i++)
 	{
