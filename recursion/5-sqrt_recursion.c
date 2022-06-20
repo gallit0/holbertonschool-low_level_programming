9e9c67082c2f10d4f8e11102eb63e421fc2f4d8f
gallit0 <mateogesede@gmail.com>
2022-06-20 07:25:24 -0700

    1

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - square root
+ * @n: int
+ * Return: int
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0 && n % 2 != 0)
+	{
+		return (-1);
+	} else if (n == 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (sqrt_check(n, 0));
+	}
+}
+/**
+* sqrt_check - checks sqrt of n
+* @n: int
+* @i: int
+*Return: int
+*/
+int sqrt_check(int n, i)
+{
+	if (i * i > n)
+	{
+		return (-1);
+	} else if (i * i == n)
+	{
+		return (i);
+	}
+	else
+	{
+		return (sqrt_check(n, i + 1));
+	}
