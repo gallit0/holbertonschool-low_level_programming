9cd11d473aafd4c80b10ac9ebf399e576468bb61
gallit0 <mateogesede@gmail.com>
2022-06-20 05:14:35 -0700

    1

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -10,10 +10,10 @@ int _strlen_recursion(char *s)
 {
 	int i = 0;
 
-	if (*s)
+	if (*s == 0)
 	{
-		i++;
-		_strlen_recursion(s++);
+		return (0);
 	}
-	return (i);
+	else
+		return (1 + _strlen_recursion(s) + 1);
 }
