eb3644100de34715f43df0c8c2d348bd178d36cf
gallit0 <mateogesede@gmail.com>
2022-06-20 09:39:43 -0700

    1

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * is_prime_number - checks for prime number n
+ * @n: number
+ * Return: int
+ */
+
+int is_prime_number(int n)
+{
+	if (n < 2)
+		return (0);
+	else
+	{
+		return (prime(n/n);
+	}
+}
+
+/**
+ * prime - checks for prime number
+ * @n: number
+ * Return: int
+ */
+
+int prime(int i)
+{
+	if ((i == 1)
+		return (1);
+	else
+		return (0);
+}
