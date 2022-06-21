98b53875d83c3e3695220f9b81d527ab55d8eb52
gallit0 <mateogesede@gmail.com>
2022-06-21 11:08:20 -0700

    1

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -13,15 +13,16 @@ int main(int argc, char **argv)
 	int i;
 	int res = 1;
 
-	if (argc == 0)
+	if (argc < 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
 	for (i = 1; i < argc; i++)
 	{
+
 		res *= atoi(argv[i]);
 	}
 	printf("%d\n", res);
 	return (0);
-} 
+}
