7b96ea67083f925229604a2a48147d2964b040a5
gallit0 <mateogesede@gmail.com>
2022-06-21 09:45:51 -0700

    1

---
+++ argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print arguments
+ * @argc: argc
+ * @argv: argv
+ * Return: 0;
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	for (i = 0; i < argc; i++)
+		printf("%d\n", argv[i]);
+	return (0);
+}
