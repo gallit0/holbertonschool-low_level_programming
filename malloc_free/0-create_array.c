c2748655ad98f3ad1e850dad10534a996ef9cc57
gallit0 <mateogesede@gmail.com>
2022-06-22 12:54:11 -0700

    1

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (0);
-	str = (char *) malloc(size * (sizeof(char)));
+	str = (char *) malloc(sizeof(char) * size);
 	while (i < size)
 	{
 		str[i] = c;
