28b21e80197f32a37480f327fb67572ab1f8f390
gallit0 <mateogesede@gmail.com>
2022-06-22 13:15:51 -0700

    1

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strdup - return pointer to allocated memory
+ * @str: string
+ * Return:char
+ */
+
+char *_strdup(char *str)
+{
+	int sizeStr = strlen(str);
+	char *str2;
+	int i;
+
+	if (str = NULL)
+		return (0);
+	str2 = malloc(strlen);
+	
+	for (str2 == NULL)
+		return (0);
+	for (i = 0; i < strlen; i++)
+		str2[i] = str[i];
+	free(str);
+	return (str2);
+}
