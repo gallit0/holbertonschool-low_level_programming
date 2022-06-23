568a0317058b882b444ff789c8c4dbce9e33aa98
gallit0 <mateogesede@gmail.com>
2022-06-23 06:10:49 -0700

    1

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -17,7 +17,7 @@ char *str_concat(char *s1, char *s2)
 	sLen = strlen(s1);
 	sLen += strlen(s2);
 
-	str = malloc(sizeof(char) * sLen);
+	str = malloc((sizeof(char) * sLen) + 1);
 	if (str == NULL)
 		return (0);
 	for (i = 0; s1[i]; i++)
