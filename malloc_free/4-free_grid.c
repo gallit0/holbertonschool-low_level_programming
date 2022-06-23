112c63f6f24d63e8f4b09ffc8fb4ed6da31eb81e
gallit0 <mateogesede@gmail.com>
2022-06-23 15:34:24 -0700

    1

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -1,14 +1,16 @@
 #include "main.h"
 
-
+/**
+ * free_grind - frees a grind
+ * @grind: array
+ * @height: int
+ */
 
 void free_grid(int **grid, int height)
 {
 	int i;
 
 	for (i = 0; i < height; i++)
-	{
 		free(grid[i]);
-	}
 	free(grid);
 }
