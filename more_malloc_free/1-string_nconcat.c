94f413d695ddbdb521a0203d88f1318979c909d0
gallit0 <mateogesede@gmail.com>
2022-06-24 07:11:38 -0700

    1

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * string_nconcat - string concatenate
+ * @s1: string
+ * @s2: string
+ * @n: int
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *p;
+	int i;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	if (n > strlen(s2))
+		n = strlen(s2);
+
+	p = malloc(n + (strlen(s1)));
+	if (p == NULL)
+		return (0);
+
+	while(*s1)
+	{
+		*p = *s1;
+		s1++;
+		p++;
+	}
+	while(*s2)
+	{
+		*p = *s2;
+		s2++;
+		p++;
+	}
+	return (p);
+}
