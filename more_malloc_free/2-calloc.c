3da7331c077a6aef1be0e6c52794fdadbcc59947
gallit0 <mateogesede@gmail.com>
2022-06-24 09:10:29 -0700

    1

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -10,11 +10,16 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *p;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		exit(0);
 	p = malloc(nmemb * size);
 	if (p == NULL)
 		exit(0);
+	for (i = 0; i < (size * mnemb); i++)
+	{
+		p[i] = 0;
+	}
 	return (p);
 }
