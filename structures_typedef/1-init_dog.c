ef77dab3bf6b7344372083c03cd13d1727f50757
gallit0 <mateogesede@gmail.com>
2022-06-27 16:12:42 -0700

    1

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * init_dog - dog :)
+ * @d: dog :)
+ * @name: dog :)
+ * @age: dog :)
+ * @owner: dog :)
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d.name = name;
+	d.age = age;
+	d.owner = owner;
+}
