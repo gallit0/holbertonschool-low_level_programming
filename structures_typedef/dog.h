689b5c3a00059e60883ca0369eb2748493c218be
gallit0 <mateogesede@gmail.com>
2022-06-28 09:35:45 -0700

    1

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -30,5 +30,5 @@ typedef struct dog_t
 int main(void);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
+struct dog_t *new_dog(char *name, float age, char *owner);
 #endif
