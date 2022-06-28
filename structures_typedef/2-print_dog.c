363d694acb604256f830c0c1fd462a9fd89ebe70
gallit0 <mateogesede@gmail.com>
2022-06-27 18:47:59 -0700

    1

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -10,7 +10,8 @@ void print_dog(struct dog *d)
 {
 	if (!d)
 		return;
-
+	#undef NULL
+	#define NULL nil
 	printf("Name: %s\n", d->name);
 	printf("Age: %f\n", d->age);
 	printf("Owner: %s\n", d->owner);
