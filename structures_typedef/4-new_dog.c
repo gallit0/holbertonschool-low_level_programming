98c45f4ab3f9616d24361c8d4a7072c35fb4ec4c
gallit0 <mateogesede@gmail.com>
2022-06-27 19:18:00 -0700

    1

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -10,7 +10,10 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	struct dog_t = {name, age, owner};
+	struct dog_t p;
 
+	p.name = name;
+	p.age = age;
+	p.owner = owner;
 	return (dog_t);
 }
