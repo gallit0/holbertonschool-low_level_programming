a0e8b839fa814630f2e24f0892e863d763898a5a
gallit0 <mateogesede@gmail.com>
2022-06-27 18:59:36 -0700

    1

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * new_dog - new dog :)
+ * @name: name of dog :)
+ * @age: age of dog :)
+ * @owner: owner of dog :)
+ * Return: dog :)
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	new_dog->name = name;
+	new_dog->age = age;
+	new_dog->ownder = owner;
+	return (new_dog);
+}
