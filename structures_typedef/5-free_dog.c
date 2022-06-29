a42952151290a1cd9919e166974e2415d12705c8
gallit0 <mateogesede@gmail.com>
2022-06-28 17:37:32 -0700

    1

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,11 @@
+#include "dog.h"
+
+/**
+ * free_dog - frees a dog :)
+ * @d: the dog :)
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d);
+}
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -32,4 +32,5 @@ int main(void);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 struct dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
