97657b1237d5ea2347aa27d38af57177a5bf9fca
gallit0 <mateogesede@gmail.com>
2022-06-29 21:11:52 -0700

    1

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,34 @@
+#include "3-calc.h"
+
+/**
+ * get_op_func - get operation function
+ * @s: string
+ * @a: int
+ * @b: int
+ * Return: int
+ */
+
+int (*get_op_func(char *s))(int a, int b)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (ops[i].op != NULL)
+	{
+		if (ops[i].op == s)
+		{
+			printf("%d\n", ops[i].f(a, b);
+			return (0);
+		}
+		i++;
+	}
+	printf("Error\n");
+	return (0);
+}
---
+++ function_pointers/3-main.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+
+/**
+ * main - main function
+ * @argc: counter to argv
+ * @argv: argv
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int a, b;
+
+	if (argc < 4)
+	{
+		printf("Error\n");
+		return (0);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	get_op_func(argv[2], a, b);
+
+	return (0);
+}
