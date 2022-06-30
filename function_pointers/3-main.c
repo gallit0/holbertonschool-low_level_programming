42ada1b039688e0f0e5d21694cdba60961c509c4
gallit0 <mateogesede@gmail.com>
2022-06-30 05:15:42 -0700

    1

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -11,19 +11,25 @@ int main(int argc, char *argv[])
 {
 	int a, b;
 
-	if (argc < 4 || (argv[2] != '%' && argv[2] != '*' && argv[2] != '+'))
+	if (argc < 4 || (*argv[2] != '%' && *argv[2] != '*' && *argv[2] != '+'))
 	{
 		printf("Error\n");
 		return (0);
 	}
-	if (argv[2] != '/' && argv[2] != '-')
+	if (*argv[2] != '/' && *argv[2] != '-')
 	{
 		printf("Error\n");
+		return (0);
 	}
 
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
-	result = (*get_op_func(argv[2])(a, b);
+	if (b == 0)
+	{
+		printf("Error\n");
+		return (0);
+	}
+	result = (*get_op_func(*argv[2])(a, b);
 	printf("%d\n", result);
 	return (0);
 }
