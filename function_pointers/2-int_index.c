b5e35de4624a96ac7036c129508dbd203f559050
gallit0 <mateogesede@gmail.com>
2022-06-29 19:31:26 -0700

    1

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - pointer to function
+ * @array: array of ints
+ * @size: int
+ * @cmp: pointer to function
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+	int result;
+
+	if (size > 0 && !array && !cmp)
+		return (-1);
+	for (; i < size; i++)
+	{
+		result = (*cmp)(array[i]);
+		if (result == 1)
+			return(i);
+	}
+	return(-1);
+
+}
