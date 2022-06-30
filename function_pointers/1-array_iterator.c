16ecb88af37bb1ade19e688b14a7bfd35a0b034d
gallit0 <mateogesede@gmail.com>
2022-06-29 18:58:59 -0700

    1

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -2,8 +2,24 @@
 
 /**
  * array_iterator - execute a function given as a parameter
+ * @array: array of ints
+ * @size: size_t
+ * @action: function
+ */
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	
+	char *p;
+	int i;
+
+	if (array == NULL || action == NULL)
+		return;
+	if (size > strlen(array))
+		size == strlen(array);
+	p = malloc(size + 1);
+	if (p == NULL)
+		return;
+	for (i = 0; p[i]; i++)
+		p[i] = array[i];
+	(*action)(p);
 }
