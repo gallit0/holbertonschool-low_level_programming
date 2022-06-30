56a6cb064a30ad04268a7a8969cb7479d4389ae7
gallit0 <mateogesede@gmail.com>
2022-06-30 05:46:25 -0700

    1

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -24,11 +24,9 @@ int (*get_op_func(char *s))(int, int)
 	{
 		if (ops[i].op == s)
 		{
-			printf("%d\n", ops[i].f(a, b);
-			return (0);
+			return (ops[i].f);
 		}
 		i++;
 	}
-	printf("Error\n");
-	return (0);
+	return (NULL);
 }
--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -11,25 +11,25 @@ int main(int argc, char *argv[])
 {
 	int a, b, result;
 
-	if (argc < 4 || (*argv[2] != '%' && *argv[2] != '*' && *argv[2] != '+'))
+	if (argc < 4)
 	{
 		printf("Error\n");
-		return (0);
+		exit(98);
 	}
-	if (*argv[2] != '/' && *argv[2] != '-')
+	if (get_op_func(argv[2]) == NULL)
 	{
 		printf("Error\n");
-		return (0);
+		exit(99);
 	}
 
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
-	if (b == 0)
+	if ((argv[2] == '/' || argv[2] == '%') && b == 0)
 	{
 		printf("Error\n");
-		return (0);
+		exit(100);
 	}
-	result = (*get_op_func(argv[2])(a, b);
+	result = get_op_func(argv[2])(a, b);
 	printf("%d\n", result);
 	return (0);
 }
