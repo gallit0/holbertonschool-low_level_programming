a345b4f0e63844526bd9562d49dca66d8986533d
gallit0 <mateogesede@gmail.com>
2022-07-01 06:25:39 -0700

    1

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - print a string :p
+ * @separator: string
+ * @n: unsigned int
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list ptr;
+
+	if (separator == NULL)
+		separator = "";
+
+	va_start(ptr, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%s", va_arg(ptr));
+		if (i != (n - 1))
+			printf("%s", separator);
+	}
+	putchar(10);
+	va_end(ptr);
+}
