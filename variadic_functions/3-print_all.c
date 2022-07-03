7b639e096b16a0d79aa3ea40f39dd4e811a89e1e
gallit0 <mateogesede@gmail.com>
2022-07-03 15:45:48 -0700

    1

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -60,32 +60,3 @@ void print_all(const char * const format, ...)
 	va_end(ptr);
 	putchar(10);
 }
-
-/**
- * print - prints
- * @format: char
- */
-
-void print(char format, ...)
-{
-	va_list word;
-
-	va_start(word, 1);
-	switch (format)
-	{
-		case 'c':
-			printf("%c", va_arg(ptr, int));
-			break;
-		case 'i':
-			printf("%d", va_arg(ptr, int));
-			break;
-		case 'f':
-                	printf("%f", va_arg(ptr, double));
-			break;
-		case 's':
-			printf("%s", va_arg(ptr, char *));
-			break;
-		default:
-			break;
-	}
-}
