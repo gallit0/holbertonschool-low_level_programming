43af774ae9e4c7a2e0eabe8c6689214aea8a01fd
gallit0 <mateogesede@gmail.com>
2022-07-03 15:43:32 -0700

    1

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -15,39 +15,50 @@ void print_all(const char * const format, ...)
 	int len;
 
 	if (format == NULL)
+	{
+		putchar(10);
 		return;
+	}
 	len = strlen(format);
 	va_start(ptr, format);
-	va_start(ptr2, format);
-	va_start(ptr3, format);
 	while (i < len)
 	{
-		switch (format[i])
-		{
-			case 'c':
-				printf("%c", va_arg(ptr, int));
-				break;
-			case 'i':
-				printf("%d", va_arg(ptr, int));
-				break;
-			case 'f':
-				printf("%f", va_arg(ptr, double));
-				break;
-			case 's':
-				printf("%s", va_arg(ptr, char *));
-				break;
-			default:
-				i++;
-				continue;
-		}
-		if(i != (len - 1))
+		print(format[i], va_arg);
+		if (i != (len - 1))
 		{
 			printf(", ");
 		}
 		i++;
 	}
 	va_end(ptr);
-	va_end(ptr2);
-	va_end(ptr3);
 	putchar(10);
+}
+
+/**
+ * print - prints
+ * @format: char
+ */
+
+void print(char format, ...)
+{
+	va_list word;
+
+	va_start(word, 1);
+	switch (format)
+	{
+		case 'c':
+			printf("%c", va_arg(ptr, int));
+			break;
+		case 'i':
+			printf("%d", va_arg(ptr, int));
+			break;
+		case 'f':
+                	printf("%f", va_arg(ptr, double));
+			break;
+		case 's':
+			printf("%s", va_arg(ptr, char *));
+			break;
+		default:
+			break;
+	}
 }
