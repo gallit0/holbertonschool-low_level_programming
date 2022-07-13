7f52ac58ce167bbbfaaacacfcad6591aed0938a5
gallit0 <mateogesede@gmail.com>
2022-07-13 09:26:55 -0700

    1

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -9,12 +9,10 @@ void free_list(list_t *head)
 {
 	list_t *temp = head;
 
-	while (temp->next != NULL && temp != NULL)
+	while (temp->next != NULL)
 	{
 		temp = temp->next;
 		free(head);
 		head = temp;
 	}
-	free(temp);
-	free(head);
 }
