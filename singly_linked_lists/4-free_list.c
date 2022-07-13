5cfe3be1cdb71ed9e5edeecc309a6dc77bd5dbb2
gallit0 <mateogesede@gmail.com>
2022-07-13 09:09:38 -0700

    1

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_list - free a list_t
+ * @head: head of the list_t
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp = head;
+
+	while (temp != NULL)
+	{
+		temp = temp->link;
+		free(head);
+		head = temp;
+	}
+}
