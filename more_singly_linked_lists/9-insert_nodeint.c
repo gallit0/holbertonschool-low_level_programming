df72bd989c29182c1fa72fd1efe3d00215cb75d2
gallit0 <mateogesede@gmail.com>
2022-07-14 13:03:24 -0700

    1

---
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - inser node at given position
+ * @head: head of list
+ * @idx: index
+ * @n: int
+ * Return: list
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+
+}
