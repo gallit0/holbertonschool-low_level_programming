e7d1f00c74be5aae338e05d5135311b828272703
gallit0 <mateogesede@gmail.com>
2022-07-14 11:07:21 -0700

    1

---
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - add node at beggining
+ * @head: head of list
+ * @n: int
+ * Return: list
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *p;
+
+	p = malloc(sizeof(listint_t));
+	if(!p)
+		return (0);
+	p->n = n;
+	p->next = (*head);
+
+	(*head) = p;
+
+	return (*head);
+}
--- more_singly_linked_lists/lists.h
+++ more_singly_linked_lists/lists.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -14,4 +15,5 @@ typedef struct listint_s
 } listint_t;
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
 #endif
