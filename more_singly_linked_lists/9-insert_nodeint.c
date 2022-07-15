80a63854a60ae5e06f5690af99330a031c342408
gallit0 <mateogesede@gmail.com>
2022-07-15 09:07:22 -0700

    1

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -10,5 +10,22 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
+	listint_t *p0 = (*head);
+	listint_t *p1;
 
+	p1 = malloc(sizeof(listint_t));
+	if (!p1)
+		return (0);
+	p1->n = n;
+	p1->next = NULL;
+
+	idx--;
+	while(idx != 1)
+	{
+		p0 = p0->next;
+		idx--;
+	}
+
+	p1->next = p0->next;
+	p0->next = p1;
 }
