eb4b7fdb5c97e06c23d590b3ce7b28c22208177d
gallit0 <mateogesede@gmail.com>
2022-07-15 09:22:23 -0700

    1

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -20,7 +20,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	p1->next = NULL;
 
 	idx--;
-	while (idx > 1)
+	while (idx < 1)
 	{
 		p0 = p0->next;
 		idx--;
@@ -29,5 +29,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	p1->next = p0->next;
 	p0->next = p1;
 
-	return (p0);
+	return (p1);
 }
