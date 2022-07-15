e5fd7511c15d86ad0c0773bcca1622e448c6a7bf
gallit0 <mateogesede@gmail.com>
2022-07-15 11:30:36 -0700

    1

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -17,6 +17,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (-1);
 	if (index == 0)
 	{
+		p = p->next;
 		return (1);
 	}
 	for (i = 0; p != NULL && i <= index; i++)
