ec966386ff3485f167b87d43245755b275ed82b3
gallit0 <mateogesede@gmail.com>
2022-07-15 09:56:40 -0700

    1

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -28,7 +28,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		if (i == idx - 1)
 		{
 			temp->next = p->next;
-			return (idx);
+			return (*head);
 		}
 		p = p->next;
 	}
