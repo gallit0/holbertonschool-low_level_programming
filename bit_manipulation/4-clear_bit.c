3146d9c4213169b9b0be7e507d56aaca83b66b4b
gallit0 <mateogesede@gmail.com>
2022-07-18 16:36:15 -0700

    1

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -9,12 +9,9 @@
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int temp = 1;
-
 	if (index >= 32)
 		return (-1);
 
-	temp <<= index;
-	(*n) ^= temp;
+	(*n) |= 1UL << index;
 	return (1);
 }
