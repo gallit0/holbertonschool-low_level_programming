e206f30801c81d5158c33934d61e4548b26e4f5f
gallit0 <mateogesede@gmail.com>
2022-07-18 14:42:51 -0700

    1

--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 #include <stdio.h>
 #include <string.h>
+#include <limits.h>
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 unsigned int power(unsigned int x, unsigned int y);
