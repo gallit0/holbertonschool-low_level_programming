a0bfc2b2e3b8dfbeb90299f27fbb1f72eed615e3
gallit0 <mateogesede@gmail.com>
2022-07-18 13:11:24 -0700

    1

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -20,13 +20,13 @@ unsigned int power(unsigned int x, unsigned int y)
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int i;
+	unsigned int i = strlen(b);
 	unsigned int n = 0;
 
 	if (!b)
 		return (0);
 
-	for (i = 0; b[i]; i++)
+	for (; i >= 0; i--)
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
