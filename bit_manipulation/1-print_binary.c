9de353cc6a5eef366a854103c9282e4a09188810
gallit0 <mateogesede@gmail.com>
2022-07-18 14:08:16 -0700

    1

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -7,11 +7,7 @@
 
 void print_binary(unsigned long int n)
 {
-	if (n < 2)
-	{
-		putchar(n + '0');
-		return;
-	}
-	print_binary(n / 2);
-	putchar('0' + n % 2);
+	if (n > 1)
+		print_binary(n >> 1);
+	putchar('0' + (n & 1));
 }
--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -4,4 +4,5 @@
 #include <string.h>
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
+unsigned int power(unsigned int x, unsigned int y);
 #endif
