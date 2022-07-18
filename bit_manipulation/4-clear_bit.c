d0afc8aeff7e2e14f78a2c0a462a5ef3f8a4d0e0
gallit0 <mateogesede@gmail.com>
2022-07-18 15:32:54 -0700

    1

---
+++ bit_manipulation/4-clear_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * clear_bit - sets value of bit to 0
+ * @n: number
+ * @index: position
+ * Return: number
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	int temp = 1;
+
+	if (index >= 32)
+		return (-1);
+
+	temp <<= index;
+	(*n) -= temp;
+	return (1);
+}
--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -8,4 +8,5 @@ void print_binary(unsigned long int n);
 unsigned int power(unsigned int x, unsigned int y);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
 #endif
