193ea01f8ec7c57dcade62a83598092c75c3bbe4
gallit0 <mateogesede@gmail.com>
2022-07-19 14:35:38 -0700

    1

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - append text to end of file
+ * @filename: name of file :p
+ * @text_content: new content
+ * Return: int
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, i;
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_RDWR);
+	for (i = 0; text_content && text_content[i]; i++)
+	{
+		if (write(fd, &text_conmtent[i], 1) < 0)
+			return (-1);
+	}
+	close(fd);
+	return (1);
+}
