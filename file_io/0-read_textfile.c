debb50aa3dd24d7f9acb8319e136dd3e8fa35d53
gallit0 <mateogesede@gmail.com>
2022-07-19 10:54:05 -0700

    1

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -23,7 +23,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	while (c != EOF && counter < l)
 	{
 		c = fgetc(p);
-		putchar (c);
+		fprintf("%c", c);
 		counter++;
 	}
 	fclose(p);
