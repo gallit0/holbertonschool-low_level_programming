0fea422ffc7a775dc7fe5069357966d87b528c61
gallit0 <mateogesede@gmail.com>
2022-07-19 14:08:25 -0700

    1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -11,7 +11,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, i;
 
-	if (!filename || !text_content)
+	if (!filename)
 		return (-1);
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
