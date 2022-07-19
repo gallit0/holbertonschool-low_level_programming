437d57a91f3aae25eda334ff5a3648d0cae9dd58
gallit0 <mateogesede@gmail.com>
2022-07-19 15:07:28 -0700

    1

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -9,16 +9,16 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, i;
+	int fd, i, slen;
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_WRONLY, O_TRUNC);
-	for (i = 0;text_content && text_content[i]; i++)
-	{
-		if (write(fd, &text_content[i], 1) < 0)
-			return (-1);
-	}
+	fd = open(filename, O_WRONLY, O_APPEND);
+	if (!text_content)
+		return (1)
+	slen = strlen(text_content);
+	if (write(fd, &text_content[i], slen) < 0)
+		return (-1);
 	close(fd);
 	return (1);
 }
