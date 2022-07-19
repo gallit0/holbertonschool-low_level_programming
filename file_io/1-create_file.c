d65cebb4004628b6b6b583ea3a4d020190a57080
gallit0 <mateogesede@gmail.com>
2022-07-19 14:08:54 -0700

    1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
-	for (i = 0; text_content[i]; i++)
+	for (i = 0; text_content && text_content[i]; i++)
 	{
 		if (write(fd, &text_content[i], 1) < 0)
 			return (-1);
