414a11c956886eaed4b0ba86eaa7f4c4b495f899
gallit0 <mateogesede@gmail.com>
2022-07-19 07:02:13 -0700

    1

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * read_textfile - read a text file
+ * @filename: name of the file :p
+ * @letters: amont of letters to print
+ * Return: letters printed
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+
+	if (!filename)
+		return (0);
+
+}
