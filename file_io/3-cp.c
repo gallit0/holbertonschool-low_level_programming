23589a151430a9f03cb1c35be583f90d729b1e5c
gallit0 <mateogesede@gmail.com>
2022-07-20 17:59:10 -0700

    1

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
 	}
 	if (r == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	if (close(o) == -1 || close(o1) == -1)
