fffcd4ecc43dfe389b4489a08afb3812f8816de2
gallit0 <mateogesede@gmail.com>
2022-07-21 17:35:48 -0700

    1

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -33,7 +33,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			temp->next = p->next;
 			temp->prev = p;
 			p->next = temp;
-			if (p->next != NULL)
+			if (temp->next != NULL)
 			{
 				p = p->next;
 				t2 = p;
