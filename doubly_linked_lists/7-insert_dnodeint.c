21ae082123a298194c9b1d04cb52464864372240
gallit0 <mateogesede@gmail.com>
2022-07-21 19:23:37 -0700

    1

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -11,7 +11,7 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *p = *h;
-	dlistint_t *temp;
+	dlistint_t *new;
 	unsigned int i = 0;
 
 	if (idx == 0)
@@ -20,20 +20,18 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		if (i == idx - 1)
 		{
-			if (p->next == NULL)
+			if (!p->next)
 				return (add_dnodeint_end(h, n));
-
-			temp = malloc(sizeof(dlistint_t));
-			if (!temp)
+			new = malloc(sizeof(dlistint_t));
+			if (!new)
 				return (0);
-			temp->n = n;
-			temp->next = p->next;
-			temp->prev = p;
-			p->next = temp;
-			p = p->next;
-			p = p->next;
-			p->prev = temp;
-			return (temp);
+			new->n = n;
+			new->next = p->next;
+			new->prev = p;
+			p->next = new;
+			p = new->next;
+			p->prev = new;
+			return (new);
 		}
 		p = p->next;
 		i++;
