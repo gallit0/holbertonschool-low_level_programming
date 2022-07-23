017860b9511f9f15fadac60226f5baa3309ef8b0
gallit0 <mateogesede@gmail.com>
2022-07-22 17:31:21 -0700

    1

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -16,11 +16,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
-	for (; idx > 1 ; idx--)
+	for (; idx != 1 ; idx--)
 	{
 		p = p->next;
 		if (!p)
-			return (0);
+		return (0);
 	}
 
 	if (!p->next)
